91907700e44edfe19bf4391fee88f8a46a4fb114
020697Dmv <dmaciasv@uqvirtual.edu.co>
2020-05-07 19:05:12 -0500

    bat request
    07 e mayo 2015

---
+++ Assets/Segundo/Script/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed= 4f;
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mov= new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0
+        );
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            transform.position + mov,
+            speed * Time.deltaTime
+        );
+
+        anim.SetFloat("MovX",mov.x);
+        anim.SetFloat("MovY",mov.y);
+    }
+}
